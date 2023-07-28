b9ad049b80657d5d2ae3eca5335f4e5f19f54514
Anastasia <anastasiakosovets@gmail.com>
2023-07-28 22:58:53 +0200

    feat: add spinner and upButton to Favorite section

--- src/common/FavoritesCard/FavoritesCard.jsx
+++ src/common/FavoritesCard/FavoritesCard.jsx
@@ -5,11 +5,14 @@ import { userData } from "../../pages/userSlice";
 import { getMyFavorites } from "../../services/apiCalls";
 import { Col, Container, Row } from "react-bootstrap";
 import { humanDate } from "../../services/useful";
+import { ScrollTopButton } from "../../common/scrollTop";
+import spinner from "../../../img/spinner.gif"
 
 export const FavoriteCard = () => {
   const user = useSelector(userData);
   const token = useSelector((state) => state.user.credentials.token);
   const [favorites, setFavorites] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     loadFavorites();
@@ -25,27 +28,43 @@ export const FavoriteCard = () => {
       }
     } catch (error) {
       console.log("Error en la solicitud AJAX: " + error);
+    } finally {
+        setLoading(false);
     }
   };
 
   return (
     <div className="generalStyleFav" style={{ fontFamily: "Great Vibes" }}>
       <Container className="genFavStyle">
-        <Row className="rowFavStyle">
-          <Col>
-            <h1 className="colorColStyle">Mis favoritos</h1>
-          </Col>
-        </Row>
-        <Row className="rowFavStyle">
-          {favorites.map((favorite) => (
-            <Col key={favorite.id} xs={12} md={5} className="favColCardStyle">
-              <h1 className="colorColSt">{favorite.concert.title}</h1>
-              <p>Date: {humanDate(favorite.concert.date)}</p>
-              <p>Group Name: {favorite.concert.groupName}</p>
+        {loading ? (
+          <Row className="rowFavStyle">
+            <Col>
+              <p>
+                <img src={spinner} alt="Loading..." className="" />
+              </p>
             </Col>
-          ))}
-        </Row>
+          </Row>
+        ) : (
+          <>
+            <Row className="rowFavStyle">
+              <Col>
+                <h1 className="colorColStyle">Mis favoritos</h1>
+              </Col>
+            </Row>
+            <Row className="rowFavStyle">
+              {favorites.map((favorite) => (
+                <Col key={favorite.id} xs={12} md={5} className="favColCardStyle">
+                  <h1 className="colorColSt">{favorite.concert.title}</h1>
+                  <p>Date: {humanDate(favorite.concert.date)}</p>
+                  <p>Group Name: {favorite.concert.groupName}</p>
+                </Col>
+              ))}
+            </Row>
+          </>
+        )}
+        <ScrollTopButton />
       </Container>
     </div>
   );
+  
 };
