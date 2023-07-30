90e998883887225b03b0b1e6975f4f08f0ae66ea
Anastasia <anastasiakosovets@gmail.com>
2023-07-30 20:47:04 +0200

    feat: add links to icon of github and linkedin

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 import "./Footer.css";
+import github from "../../../img/github.png";
+import linkedin from "../../../img/linkedin.png";
 
 export const Footer = () => {
   return (
     <div className="footerStyle" style={{ fontFamily: "Great Vibes" }}>
-  <div>
-    <p className="txtFooter">© Anastasia Kosovets </p>
-  </div>
-  <div className="txtFooter">
-    <p>Aviso legal</p>
-    <p>Política de privacidad</p>
-  </div>
-</div>
+      <div>
+        <p className="txtFooter">© Anastasia Kosovets </p>
+      </div>
+      <div>
+        <p>
+          <a
+            href={"https://github.com/AnastasiaKosovets"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={github} alt="github link" className="iconFooter"/>
+          </a>
+        </p>
+      </div>
+      <div>
+        <p>
+          <a
+            href={"https://www.linkedin.com/in/anastasia-kosovets-00022917b/"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedin} alt="github link" className="iconFooter" />
+          </a>
+        </p>
+      </div>
+      <div className="txtFooter">
+        <p>Aviso legal</p>
+        <p>Política de privacidad</p>
+      </div>
+    </div>
   );
 };
